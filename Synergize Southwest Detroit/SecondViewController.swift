a8fe39059455e3159a93ef047b63796b84c6c7de
Giancarlo Anemone <ganemone@gmail.com>
2015-04-09 12:37:35 -0400

    Initial commit

---
+++ Synergize Southwest Detroit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Synergize Southwest Detroit
+//
+//  Created by Giancarlo Anemone on 4/9/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
