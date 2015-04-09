34f7f3596e367da4f5fbd8f7c824613d1d284b96
Giancarlo Anemone <ganemone@gmail.com>
2015-04-09 15:18:14 -0400

    Add bridging header and constants file

---
+++ Synergize Southwest Detroit/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  Synergize Southwest Detroit
+//
+//  Created by Giancarlo Anemone on 4/9/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+#ifndef Synergize_Southwest_Detroit_BridgingHeader_h
+#define Synergize_Southwest_Detroit_BridgingHeader_h
+
+
+#endif
---
+++ Synergize Southwest Detroit/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Synergize Southwest Detroit
+//
+//  Created by Giancarlo Anemone on 4/9/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+import Foundation
