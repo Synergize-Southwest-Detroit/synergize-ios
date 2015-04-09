e26002d84d5a88f3721abf8b3959ba7b3b3d0ffc
Giancarlo Anemone <ganemone@gmail.com>
2015-04-09 13:24:01 -0400

    Add class for event table view cell

---
+++ Synergize Southwest Detroit/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  Synergize Southwest Detroit
+//
+//  Created by Giancarlo Anemone on 4/9/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
